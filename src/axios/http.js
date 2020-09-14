c2f87605fa3ed7d4d422cd2c584c7a9b3e797a22
赖平泉 <416174289@qq.com>
2020-09-14 09:33:29 +0800

    1

--- src/axios/http.js
+++ src/axios/http.js
@@ -1,68 +1,67 @@
 import axios from 'axios'
 import qs from 'qs'
+import Vue from 'vue'
 // import store from 'store/index'
-// import { Indicator, Toast } from 'mint-ui'
 import {
     getCookie
 } from "../utils/auth";
 import router from "../router/index"
-var _this = this;
+import {
+	Message
+} from "element-ui";
 axios.defaults.timeout = 5000 // 请求超时时间
 axios.defaults.baseURL = process.env.VUE_APP_BASE_API
 axios.defaults.headers.post['Content-Type'] =
     'application/x-www-form-urlencoded;charset=UTF-8' // post请求头的设置
 // axios.defaults.headers.common["token"] = getCookie("token") ? getCookie("token") : ""
 // axios respone拦截器
-axios.interceptors.response.use(
-    response => {
-        // debugger
-        // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
-        // 否则的话抛出错误 结合自身业务和后台返回的接口状态约定写respone拦截器
-        // Indicator.close()
-        if (response.status === 200) {
-            if (response.data.code == "401") {
-                setTimeout(() => {
-                    router.push('login')
-                }, 2000);
-            } else {
-                return Promise.resolve(response)
-            }
-        } else {
-            Toast({
-                message: response.data.msg,
-                position: 'middle',
-                duration: 2000
-            });
-            return Promise.reject(response)
-        }
 
-    },
-    error => {
-        Indicator.close()
-        const responseCode = error.response.status
-        switch (responseCode) {
-            // 401：未登录
-            case 401:
-                break
-                // 404请求不存在
-            case 404:
-                Toast({
-                    message: '网络请求不存在',
-                    position: 'middle',
-                    duration: 2000
-                });
-                break
-            default:
-                Toast({
-                    message: error.response.data.message,
-                    position: 'middle',
-                    duration: 2000
-                });
-        }
-
-        return Promise.reject(error.response)
+let loading;
+axios.interceptors.request.use(config => {
+    loading = Vue.prototype.$loading({
+        lock: true,
+        text: '数据加载中,请等待...',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0,0,0,.7)'
+    });
+    return config;
+},error => {
+    loading.close();
+    return Promise.reject(err)
+})
+axios.interceptors.response.use( response => {
+    loading.close();
+    if (response.status === 200) {
+        return Promise.resolve(response)
+    } else {
+        Message.error(response.data.msg);
+        return Promise.reject(response)
     }
-)
+    }, err => {
+        if (err && err.response) {
+            switch (err.response.status) {
+                case 400: err.message = '请求错误(400)' ; break;
+                case 401: err.message = '未授权，请重新登录(401)'; break;
+                case 403: err.message = '拒绝访问(403)'; break;
+                case 404: err.message = '请求出错(404)'; break;
+                case 408: err.message = '请求超时(408)'; break;
+                case 500: err.message = '服务器错误(500)'; break;
+                case 501: err.message = '服务未实现(501)'; break;
+                case 502: err.message = '网络错误(502)'; break;
+                case 503: err.message = '服务不可用(503)'; break;
+                case 504: err.message = '网络超时(504)'; break;
+                case 505: err.message = 'HTTP版本不受支持(505)'; break;
+                default: err.message = `连接出错(${err.response.status})!`;
+            }
+        }else{
+            err.message = '连接服务器失败!'
+        }
+    Message.error(err.message);
+    sessionStorage.clear();
+    loading.close();
+    router.push("/login");
+    return Promise.reject(err);
+});
 /**
  * 封装get方法，对应get请求
  * @param {String} url [请求的url地址]
@@ -101,23 +100,16 @@ function form(url, params) {
     })
     //  或者return axios.post();
 }
+
 /**
  * post方法，对应post请求
  * @param {String} url [请求的url地址]
  * @param {Object} params [请求时携带的参数]
  */
-function post(url, params) {
-    return new Promise((resolve, reject) => {
-        axios
-            .post(url, qs.parse(params))
-            .then(res => {
-                resolve(res)
-            })
-            .catch(err => {
-                reject(err)
-            })
-    })
-    //  或者return axios.post();
+function post(url, postData, query = {}) {
+    return axios.post(url, postData, {
+        params: query,
+    });
 }
 /**
  * delete方法，对应put请求
